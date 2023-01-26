fa67b856c3eca297e90e4f85eba60ab2bec0fd97
ErikaJPB <erikajpinedab@gmail.com>
2023-01-25 21:15:38 -0500

    styles

--- components/Portfolio.jsx
+++ components/Portfolio.jsx
@@ -4,7 +4,7 @@ import { projects } from "../pages/api/data";
 function Projects() {
   return (
     <div className="relative">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 m-auto">
+      <div className="grid grid-cols-auto gap-8 m-auto">
         {projects.map((project) => (
           <div className="p-6 rounded-lg shadow-lg bg-center bg-cover object-center object-cover">
             <h1 className="text-4xl font-bold shadow-gray-400 text-center p-6">
