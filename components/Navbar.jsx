881cf2677a0331a850866e57b2df8cba290699f9
ErikaJPB <erikajpinedab@gmail.com>
2023-01-26 12:18:54 -0500

    changes

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -32,8 +32,8 @@ function Navbar() {
     >
       <div className="max-w-[1240px] m-auto flex justify-between items-center p-4 text-white">
         <Link href="/">
-          <h1 style={{ color: `${textColor}` }} className="font-bold text-3xl">
-            Developed By EJPB
+          <h1 style={{ color: `${textColor}` }} className="font-bold text-2xl">
+            Developed By ErikaJPB
           </h1>
         </Link>
         <ul style={{ color: `${textColor}` }} className="hidden sm:flex">
