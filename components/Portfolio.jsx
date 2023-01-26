e71dcf19bc32f9fb5ccca39b9e5ee3444d851ee0
ErikaJPB <erikajpinedab@gmail.com>
2023-01-25 21:05:03 -0500

    fixone

--- components/Portfolio.jsx
+++ components/Portfolio.jsx
@@ -4,10 +4,10 @@ import { projects } from "../pages/api/data";
 function Projects() {
   return (
     <div className="relative">
-      <div className="flex sm:flex-row flex-wrap space-x-8 mt-10 justify-center">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 m-auto">
         {projects.map((project) => (
-          <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 p-6 rounded-lg shadow-lg bg-center bg-cover">
-            <h1 className="text-4xl font-bold shadow-gray-400  text-center p-6">
+          <div className="p-6 rounded-lg shadow-lg bg-center bg-cover">
+            <h1 className="text-4xl font-bold shadow-gray-400 text-center p-6">
               {project.title}
             </h1>
             <p className="text-gray-600 text-center">{project.Description}</p>
