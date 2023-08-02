36c5269432a99ce837198056bf03abfba206b204
ErikaJPB <erikajpinedab@gmail.com>
2023-08-02 11:19:25 -0500

    adding spotify clone

--- components/Links.jsx
+++ components/Links.jsx
@@ -1,9 +1,6 @@
 import React from "react";
-import {
-  AiOutlineGithub,
-  AiOutlineLinkedin,
-  AiOutlineTwitter,
-} from "react-icons/ai";
+import { AiOutlineGithub, AiOutlineLinkedin } from "react-icons/ai";
+import Image from "next/image";
 
 function Links() {
   return (
@@ -19,11 +16,18 @@ function Links() {
         <AiOutlineLinkedin className="text-4xl mx-8 cursor-pointer  ml-1 my-2" />
       </a>
       <a
-        href="https://twitter.com/TheGreyMind"
+        href="https://www.threads.net/@erikajpb"
         target="_blank"
         rel="noreferrer"
       >
-        <AiOutlineTwitter className="text-4xl mx-8 cursor-pointer  ml-1 my-2" />
+        <Image
+          src="/assets/threads.png"
+          height={35}
+          width={35}
+          alt="Threads"
+          quality={100}
+          className="text-4xl mx-8 cursor-pointer rounded-3xl   ml-1 my-2"
+        />
       </a>
     </div>
   );
--- pages/api/data.js
+++ pages/api/data.js
@@ -1,6 +1,16 @@
 export const projects = [
   {
     id: 1,
+    title: "Spotify Clone",
+    Description:
+      "A responsive Spotify clone built with Next.js 13, TypeScript and TailwindCSS. The app uses Supabase for Authentication and Database. The app allows users that are suscribed to listen and create their own library. The subscription and payment method was made through Stripe.",
+    image: "/assets/spotifyclone.gif",
+    GitHub: ["https://github.com/ErikaJPB/spotify-one-clone"],
+    Deployment: "https://spotify-one-clone.vercel.app/",
+  },
+
+  {
+    id: 2,
     title: "BookCove",
     Description:
       "A book finder app that allows you to create a favorite list, you can sign up with your google account, read the description and see other details of your favorite book, and remove them from the list. The project was made with Next.js, TypeScript, TailwindCSS. Firebase was used for authentication and database.",
@@ -10,7 +20,7 @@ export const projects = [
   },
 
   {
-    id: 2,
+    id: 3,
     title: "ACMEtronics",
     Description:
       "This project was an e-commerce platform created as a group effort for the Henry Bootcamp. We created our own information to use as an API and worked with PostgreSQL as the database. The backend was developed with Node.js and Express, and the frontend was created with React, Redux, Bootstrap, and Material UI.",
@@ -22,7 +32,7 @@ export const projects = [
     Deployment: "https://acmetronics-henryproject.vercel.app/",
   },
   {
-    id: 3,
+    id: 4,
     title: "Pokemon World",
     Description:
       "This project is an individual work that was created for the Henry Bootcamp. It utilizes the PokeApi and uses PostgreSQL as the database. The backend was developed with Node.JS and Express, and the frontend was created with React, Redux, and pure CSS.",
@@ -31,7 +41,7 @@ export const projects = [
     Deployment: "https://henrypi-pokemon-ejpb.vercel.app/",
   },
   {
-    id: 4,
+    id: 5,
     title: "Todo App",
     Description:
       "A simple Todo app built with Next.js, TypeScript, and TailwindCSS. It allows users to manage their to-do list. The app uses Firebase as its database.",
@@ -40,7 +50,7 @@ export const projects = [
     Deployment: "https://todo-app-erikajpb.vercel.app/",
   },
   {
-    id: 5,
+    id: 6,
     title: "Weather App",
     Description:
       "A small front-end project utilizing Next.js, TypeScript, and TailwindCSS, which accesses the Open Weather Map API to fetch weather information. The purpose of the project was to become familiar with TypeScript and to continue practicing TailwindCSS for styling.",
